6ebe01c135adc28f078cda78373c0d7b2be79c94
Ubuntu <ubuntu@ubuntu.lan>
2021-04-21 17:36:29 +0000

    Add command to install bin and app to board

--- runner-build.sh
+++ runner-build.sh
@@ -6,3 +6,9 @@ cd ~/actions-runner/_work/tock/tock/boards/nordic/nrf52840dk/
 
 # Compile
 make
+
+# Install bin
+make install
+
+# Install Blink App
+tockloader install --board nrf52dk --jlink blink < '1'
