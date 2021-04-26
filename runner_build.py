23592229350bcbbe43cfe7aedc8d113d54b577fe
Ubuntu <ubuntu@ubuntu.lan>
2021-04-26 16:48:15 +0000

    Implement util lib

--- runner_build.py
+++ runner_build.py
@@ -7,6 +7,8 @@ try:
 except ImportError:
     from yaml import Loader
 
+from lib.util import get_board_info
+
 # Parse configuration
 with open(f"{Path.home()}/tock-test-harness/test.config.yml", 'r') as config_yml:
     TEST_CONFIG = load(config_yml, Loader=Loader)
@@ -15,18 +17,15 @@ with open(f"{Path.home()}/tock-test-harness/test.config.yml", 'r') as config_yml
 with open(f"{Path.home()}/tock-test-harness/board.map.yml", 'r') as board_map_yml:
     BOARD_MAP = load(board_map_yml, Loader=Loader)
 
-# Lookup board
-if 'env' in TEST_CONFIG:
-    if 'board' in TEST_CONFIG['env']:
-        if TEST_CONFIG['env']['board'] in BOARD_MAP:
-            # TODO: Create a subroutine to check board object
-            BOARD_MODEL = TEST_CONFIG['env']['board']
-            BOARD_INFO = BOARD_MAP[BOARD_MODEL]
-            BOARD_PATH = f"boards/{BOARD_INFO['brand']}/{BOARD_INFO['model']}"
-            TOCK_PATH = f"{Path.home()}/actions-runner/_work/tock/tock"
+# If board exists, build the corresponding board
+if BOARD_INFO := get_board_info(TEST_CONFIG, BOARD_MAP):
+    BOARD_PATH = (f"boards/{BOARD_INFO['brand']}/{BOARD_INFO['model']}"
+                  if 'brand' in BOARD_INFO
+                  else f"boards/{BOARD_INFO['model']}")
+    TOCK_PATH = f"{Path.home()}/actions-runner/_work/tock/tock"
 
-            # Change to the corresponding path of the board
-            os.chdir(f"{TOCK_PATH}/{BOARD_PATH}")
+    # Change to the corresponding path of the board
+    os.chdir(f"{TOCK_PATH}/{BOARD_PATH}")
 
-            # Compile
-            os.system('make')
+    # Compile
+    os.system('make')
